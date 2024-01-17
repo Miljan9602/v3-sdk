70a005aaacc2dfe8e130efb9266fa819ab8279ce
Miljan9602 <rakitamiljan@yahoo.com>
2024-01-17 16:27:28 +0530

    Updated to use miljan9602 version

--- src/entities/tickDataProvider.ts
+++ src/entities/tickDataProvider.ts
@@ -1,4 +1,4 @@
-import { BigintIsh } from '@uniswap/sdk-core'
+import { BigintIsh } from '@miljan9602/sdk-core'
 
 /**
  * Provides information about ticks
--- src/utils/priceTickConversions.test.ts
+++ src/utils/priceTickConversions.test.ts
@@ -1,4 +1,4 @@
-import { Price, Token } from '@uniswap/sdk-core'
+import { Price, Token } from '@miljan9602/sdk-core'
 import { tickToPrice } from './index'
 import { priceToClosestTick } from './priceTickConversions'
 
